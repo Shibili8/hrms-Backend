82b30c9174f11e3b69427209336843929da8cafb
Shibili8 <shibilishibizz99@gmail.com>
2025-11-23 19:46:48 +0530

    Backend  Updated

--- src/index.js
+++ src/index.js
@@ -15,10 +15,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://hrms-backend-4-zf5z.onrender.com/api", 
-  credentials: true   
-}));
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
