2d77705fd8b74b4d899c499b9b10b23a2ac9de1d
Shibili8 <shibilishibizz99@gmail.com>
2025-11-23 18:22:01 +0530

    Logs Action Updated

--- src/models/EmployeeTeam.js
+++ src/models/EmployeeTeam.js
@@ -5,11 +5,26 @@ const EmployeeTeam = sequelize.define(
   "employee_team",
   {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    employee_id: DataTypes.INTEGER,
-    team_id: DataTypes.INTEGER,
-    assigned_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+
+    employee_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    team_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    assigned_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   },
-  { timestamps: false }
+  {
+    timestamps: false,
+    tableName: "employee_teams", // optional (consistent naming)
+  }
 );
 
 export default EmployeeTeam;
