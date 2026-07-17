563514023d59207c8cb37da711b1a5ca6f93d8da
Shibili8 <shibilishibizz99@gmail.com>
2026-07-17 10:35:50 +0530

    Database is changed to Neon

--- src/db.js
+++ src/db.js
@@ -1,34 +1,17 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-console.log({
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_PASS: process.env.DB_PASS ? "Loaded" : "Missing",
-});
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    logging: false,
-    
-  }
-  
-);
+  },
+  logging: false,
+});
 
 export default sequelize;
