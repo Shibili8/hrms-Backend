0a208fe4bc32a8703adba09f38d97fecc231d3e9
Shibili8 <shibilishibizz99@gmail.com>
2026-07-17 10:01:40 +0530

    backend is updated

--- src/db.js
+++ src/db.js
@@ -2,10 +2,19 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log({
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_PASS: process.env.DB_PASS ? "Loaded" : "Missing",
+});
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
+
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -17,7 +26,9 @@ const sequelize = new Sequelize(
       }
     },
     logging: false,
+    
   }
+  
 );
 
 export default sequelize;
